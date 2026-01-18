9ff95dd75ea379b3fc1b470febd0345256d09ad7
Ro-LiT <rowanli.tangalin@studenti.unitn.it>
2026-01-18 10:09:41 +0100

    add screen interface header

---
+++ incl/screen.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../games/game_files/display/types.h"
+
+void set_address_window(u8 top_left_x, u8 top_left_y,u8 bottom_right_x,u8 bottom_right_y);
+void write_data(u8 data);
+
+
+
+
+
