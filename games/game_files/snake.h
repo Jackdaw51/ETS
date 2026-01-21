c5b77e473ef0a091876b7b6ef8efd379ecc74af6
Suge42 <lsandrini04@gmail.com>
2026-01-21 22:40:31 +0100

    Commited only part of the directory by error

---
+++ games/game_files/snake.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "display/types.h"
+
+typedef enum {
+	PLAYING,
+	LOST,
+	WIN,
+} State;
+
+typedef enum {
+  Q_UP,
+  Q_DOWN,
+  Q_LEFT,
+  Q_RIGHT
+} Quad_Direction;
+
+void start_snake(void);
+boolean check_crush(u8* snake_x, u8* snake_y, u8 snake_length, u8 new_head_x, u8 new_head_y);
+boolean has_eaten(u8 head_x, u8 head_y, u8 food_x, u8 food_y);
+boolean new_food(u8* food_x, u8* food_y, u8* snake_x, u8* snake_y, u8 snake_length);
