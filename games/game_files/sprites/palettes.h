625ef0d32a61f533ba6462d99b9bd20c153ef10c
Suge42 <lsandrini04@gmail.com>
2026-01-22 01:18:31 +0100

    Palettes and PNGs added, still having problem with sprite_tool

--- games/game_files/sprites/palettes.h
+++ games/game_files/sprites/palettes.h
@@ -1,21 +1,25 @@
 #pragma once
+#include <stdint.h>
 
 #define MAX_PALETTES 10
+// Indices used to recognize pallettes inside the palette array
 #define BW_INDEX 0
 #define RETRO_RBY_INDEX 1
 #define OLIVE_GREEN_INDEX 2
+#define SNAKE_INDEX 3
+#define RAT_INDEX 4
+#define APPLE_INDEX 5
 
-#include <stdint.h>
 
 typedef struct {
-	uint8_t colors[3*4];	
+	uint8_t colors[3*4];
 } Palette;
 
 typedef struct {
-	uint16_t colors[3];	
+	uint16_t colors[3];
 } Palette565;
 
-#define PALETTE_ARR_LEN 3
+//#define PALETTE_ARR_LEN 3
 
 extern const Palette565 PaletteArray565[MAX_PALETTES];
 extern const Palette PaletteArray[MAX_PALETTES];
