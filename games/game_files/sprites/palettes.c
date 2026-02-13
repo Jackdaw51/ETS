6c22c228e3968675b92a606a2d87c342ef5824fa
Ro-LiT <rowanli.tangalin@studenti.unitn.it>
2026-02-13 18:21:00 +0100

    add back the 565 colors

--- games/game_files/sprites/palettes.c
+++ games/game_files/sprites/palettes.c
@@ -72,6 +72,108 @@
     }\
 }\
 
+#define SNAKE_PALETTE_565 (Palette565){\
+	.colors = {\
+		0x0000,\
+		0x1284,\
+		0x1ba6,\
+	}\
+}\
+
+#define FIBONACCI_RAT_PALETTE_565 (Palette565){\
+	.colors = {\
+		0x0000,\
+		0x8430,\
+		0xf41f,\
+	}\
+}\
+
+#define APPLE_PALETTE_565 (Palette565){\
+	.colors = {\
+		0x0000,\
+		0xb082,\
+		0x1284,\
+	}\
+}\
+
+#define BW_565 (Palette565){\
+    .colors = {\
+        0x0000,\
+        0x52aa,\
+        0xffff,\
+    }\
+}\
+
+#define RETRO_RBY_565 (Palette565){\
+    .colors = {\
+        0xfa69,\
+        0x3679,\
+        0xfe66,\
+    }\
+}\
+
+#define PONG_CUSTOM_PALETTE_565 (Palette565){\
+    .colors = {\
+        0xce17,\
+        0xd2d5,\
+        0x5c17,\
+    }\
+}\
+
+#define OLIVE_GREEN_565 (Palette565){\
+    .colors = {\
+        0x3306,\
+        0x09c1,\
+        0x9de1,\
+    }\
+}\
+
+#define DINO_CUSTOM_PALETTE_565 (Palette565){\
+    .colors = {\
+        0xeeb5,\
+        0x4be9,\
+        0x0000,\
+    }\
+}\
+
+#define SPACESHIP_PALETTE_565 (Palette565){\
+    .colors = {\
+        0xf800,\
+        0xff20,\
+        0x07dd,\
+    }\
+}\
+
+#define ALIEN_1_PALETTE_565 (Palette565){\
+    .colors = {\
+        0x05bf,\
+        0x17e0,\
+        0xf800,\
+    }\
+}\
+
+#define ALIEN_2_PALETTE_565 (Palette565){\
+    .colors = {\
+        0x2cc0,\
+        0x1b40,\
+        0x9a1f,\
+    }\
+}\
+
+#define UFO_PALETTE_565 (Palette565){\
+    .colors = {\
+        0x94b2,\
+        0x1b40,\
+        0x879f,\
+    }\
+}\
+
+const Palette565 PaletteArray565[MAX_PALETTES] = {
+    BW_565,
+    RETRO_RBY_565,
+    OLIVE_GREEN_565,
+};
+
 const Palette PaletteArray[MAX_PALETTES] = {
 	BW,
 	RETRO_RBY,
